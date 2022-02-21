13509456ff88c337c3a599e8f771d58f399365f8
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-22 01:50:45 +0300

    add findAll methods and implement classes

--- src/main/java/com/anilakdemir/case3anilakd/usr/service/UsrUserService.java
+++ src/main/java/com/anilakdemir/case3anilakd/usr/service/UsrUserService.java
@@ -1,10 +1,22 @@
 package com.anilakdemir.case3anilakd.usr.service;
 
+import com.anilakdemir.case3anilakd.usr.entity.UsrUser;
+import com.anilakdemir.case3anilakd.usr.service.entityService.UsrUserEntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author anilakdemir
  */
 @Service
+@RequiredArgsConstructor
 public class UsrUserService {
+
+    private final UsrUserEntityService usrUserEntityService;
+
+    public List<UsrUser> findAll(){
+        return usrUserEntityService.findAll();
+    }
 }
--- src/main/java/com/anilakdemir/case3anilakd/usr/service/entityService/UsrUserEntityService.java
+++ src/main/java/com/anilakdemir/case3anilakd/usr/service/entityService/UsrUserEntityService.java
@@ -1,7 +1,24 @@
 package com.anilakdemir.case3anilakd.usr.service.entityService;
 
+
+import com.anilakdemir.case3anilakd.gen.service.BaseEntityService;
+import com.anilakdemir.case3anilakd.usr.dao.UsrUserDao;
+import com.anilakdemir.case3anilakd.usr.entity.UsrUser;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @author anilakdemir
  */
-public class UsrUserEntityService {
+@Service
+public class UsrUserEntityService extends BaseEntityService<UsrUser, UsrUserDao> {
+
+    public UsrUserEntityService (UsrUserDao dao) {
+        super(dao);
+    }
+
+    public List<UsrUser> findAll () {
+        return getDao().findAll();
+    }
 }
