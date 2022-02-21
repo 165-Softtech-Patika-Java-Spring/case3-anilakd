2bac5545e5377561692b47497318b64974353b5e
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-22 02:05:28 +0300

    add convertToUsrUserDtoList method

--- src/main/java/com/anilakdemir/case3anilakd/usr/converter/UsrUserMapper.java
+++ src/main/java/com/anilakdemir/case3anilakd/usr/converter/UsrUserMapper.java
@@ -1,8 +1,12 @@
 package com.anilakdemir.case3anilakd.usr.converter;
 
+import com.anilakdemir.case3anilakd.usr.dto.UsrUserDto;
+import com.anilakdemir.case3anilakd.usr.entity.UsrUser;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 /**
  * @author anilakdemir
@@ -11,4 +15,6 @@ import org.mapstruct.factory.Mappers;
 public interface UsrUserMapper {
 
     UsrUserMapper INSTANCE = Mappers.getMapper(UsrUserMapper.class);
+
+    List<UsrUserDto> convertToUsrUserDtoList(List<UsrUser> usrUserList);
 }
