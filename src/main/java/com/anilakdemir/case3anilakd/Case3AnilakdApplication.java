54609cb834ce65c6e658c6236bbe55a751946e1b
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-22 00:35:37 +0300

    create spring project

---
+++ src/main/java/com/anilakdemir/case3anilakd/Case3AnilakdApplication.java
@@ -0,0 +1,13 @@
+package com.anilakdemir.case3anilakd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case3AnilakdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Case3AnilakdApplication.class, args);
+	}
+
+}
