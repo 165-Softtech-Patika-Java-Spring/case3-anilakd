637d678cf2a141bed9b037c68784df83ef4b2729
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-24 04:58:44 +0300

    add UsrUserController endpoints

--- src/main/java/com/anilakdemir/case3anilakd/usr/controller/UsrUserController.java
+++ src/main/java/com/anilakdemir/case3anilakd/usr/controller/UsrUserController.java
@@ -1,9 +1,14 @@
 package com.anilakdemir.case3anilakd.usr.controller;
 
+import com.anilakdemir.case3anilakd.usr.dto.UsrUserDto;
+import com.anilakdemir.case3anilakd.usr.dto.UsrUserSaveRequestDto;
+import com.anilakdemir.case3anilakd.usr.dto.UsrUserUpdateRequestDto;
 import com.anilakdemir.case3anilakd.usr.service.UsrUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author anilakdemir
@@ -14,5 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsrUserController {
 
     private final UsrUserService usrUserService;
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody UsrUserSaveRequestDto usrUserSaveRequestDto){
+        UsrUserDto usrUserDto = usrUserService.save(usrUserSaveRequestDto);
+        return ResponseEntity.ok(usrUserDto);
+    }
+
+    @GetMapping()
+    public ResponseEntity getAll(){
+        List<UsrUserDto> usrUserDtoList = usrUserService.findAll();
+        return ResponseEntity.ok(usrUserDtoList);
+    }
+
+    @GetMapping("/id/{id}") // users?id=5 /user/id
+    public ResponseEntity getById(@PathVariable Long id){
+        UsrUserDto usrUserDto = usrUserService.findById(id);
+        return ResponseEntity.ok(usrUserDto);
+    }
+
+    @GetMapping("/username/{username}") //users?username=anil /user/username
+    public ResponseEntity getByUsername(@PathVariable String username){
+        UsrUserDto usrUserDto = usrUserService.findByUsername(username);
+        return ResponseEntity.ok(usrUserDto);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity deleteByUsernameAndPhoneNumber(@RequestParam String username, @RequestParam String phoneNumber){
+        usrUserService.deleteByUsernameAndPhoneNumber(username, phoneNumber);
+        return ResponseEntity.ok(Void.TYPE);
+    }
+
+    @PutMapping
+    public ResponseEntity updateUsrUser(@RequestBody UsrUserUpdateRequestDto usrUserUpdateRequestDto){
+        UsrUserDto usrUserDto = usrUserService.update(usrUserUpdateRequestDto);
+        return ResponseEntity.ok(usrUserDto);
+    }
 
 }
