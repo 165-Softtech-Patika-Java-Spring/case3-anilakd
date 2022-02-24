8d00ac38d8d89a8737ca1cb757027bc8a5fb6b3b
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-24 04:56:50 +0300

    add UsrUserDao some methods

--- src/main/java/com/anilakdemir/case3anilakd/usr/dao/UsrUserDao.java
+++ src/main/java/com/anilakdemir/case3anilakd/usr/dao/UsrUserDao.java
@@ -4,9 +4,21 @@ import com.anilakdemir.case3anilakd.usr.entity.UsrUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author anilakdemir
  */
 @Repository
 public interface UsrUserDao extends JpaRepository<UsrUser, Long> {
+
+    Optional<UsrUser> findByUsername (String username);
+
+    boolean existsByUsernameOrEmailOrPhoneNumber(String username, String email, String phoneNumber);
+
+    boolean existsByUsernameAndPhoneNumber(String username, String phoneNumber);
+
+    void deleteByUsernameAndPhoneNumber(String username, String phoneNumber);
+
+    boolean existsById(Long id);
 }
