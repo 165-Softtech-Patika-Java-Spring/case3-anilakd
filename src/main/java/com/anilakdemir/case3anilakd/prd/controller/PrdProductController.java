f802af8e4096f8fdffcf5e6b4a19b441b3a88fe1
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-25 04:28:35 +0300

    create PrdProductController

---
+++ src/main/java/com/anilakdemir/case3anilakd/prd/controller/PrdProductController.java
@@ -0,0 +1,58 @@
+package com.anilakdemir.case3anilakd.prd.controller;
+
+import com.anilakdemir.case3anilakd.prd.dto.PrdProductDto;
+import com.anilakdemir.case3anilakd.prd.dto.PrdProductSaveRequestDto;
+import com.anilakdemir.case3anilakd.prd.service.PrdProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author anilakdemir
+ */
+@RestController
+@RequestMapping("/v1/api/products")
+@RequiredArgsConstructor
+public class PrdProductController {
+
+    private final PrdProductService prdProductService;
+
+    @Operation(summary = "Save product")
+    @PostMapping
+    public ResponseEntity save(@RequestBody PrdProductSaveRequestDto prdProductSaveRequestDto){
+        PrdProductDto prdProductDto = prdProductService.save(prdProductSaveRequestDto);
+        return ResponseEntity.ok(prdProductDto);
+    }
+
+    @Operation(summary = "Find all products")
+    @GetMapping
+    public ResponseEntity getAll(){
+        List<PrdProductDto> prdProductDtoList = prdProductService.findAll();
+        return ResponseEntity.ok(prdProductDtoList);
+    }
+
+    @Operation(summary = "Get product by id")
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Long id){
+        PrdProductDto prdProductDto = prdProductService.findByIdWithControl(id);
+        return ResponseEntity.ok(prdProductDto);
+    }
+
+    @Operation(summary = "Delete product by id")
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id){
+        prdProductService.deleteByIdWithControl(id);
+        return ResponseEntity.ok(Void.TYPE);
+    }
+
+    @Operation(summary = "Update price of product")
+    @PatchMapping("/{id}")
+    public ResponseEntity updatePrice(@PathVariable Long id, @RequestParam BigDecimal price){
+        PrdProductDto prdProductDto = prdProductService.updatePrice(id, price);
+        return ResponseEntity.ok(prdProductDto);
+    }
+}
