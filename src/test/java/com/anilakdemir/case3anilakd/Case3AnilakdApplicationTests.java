304447897fa16af9a8f03ca2d262dc353c1e23d5
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-03-01 03:55:26 +0300

    reformat code

--- src/test/java/com/anilakdemir/case3anilakd/Case3AnilakdApplicationTests.java
+++ src/test/java/com/anilakdemir/case3anilakd/Case3AnilakdApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Case3AnilakdApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads () {
+    }
 
 }
